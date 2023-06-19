c5a4157cb7188afa97fffece324038eec2ba9352
Alberto G. Jácome <contact@agjacome.dev>
2023-06-19 12:10:43 +0200

    Tweak some LSP configs

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -11,6 +11,20 @@ lsp_zero.preset({
     suggest_lsp_servers = false,
 })
 
+lsp_zero.on_attach(function(_, buffer)
+    local opts = function(desc)
+        return { buffer = buffer, silent = true, desc = desc }
+    end
+
+    lsp_zero.default_keymaps(opts)
+    vim.keymap.set({ 'n', 'x' }, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts('Format current buffer (LSP)'))
+
+    vim.keymap.set('n', 'gd', '<cmd>Telescope lsp_definitions<cr>',      opts('Go to definition (LSP)'))
+    vim.keymap.set('n', 'gD', '<cmd>Telescope lsp_type_definitions<cr>', opts('Go to type definition (LSP)'))
+    vim.keymap.set('n', 'gi', '<cmd>Telescope lsp_implementations<cr>',  opts('Go to implementation (LSP)'))
+    vim.keymap.set('n', 'gr', '<cmd>Telescope lsp_references<cr>',       opts('Find references (LSP)'))
+end)
+
 local neodev_status_ok, neodev = pcall(require, 'neodev')
 if neodev_status_ok then
     neodev.setup()
@@ -18,15 +32,31 @@ end
 
 lsp_zero.setup()
 
+local cmp        = require('cmp')
+local cmp_action = lsp_zero.cmp_action()
+
+cmp.setup({
+    sources = {
+        { name = 'nvim_lsp' },
+        { name = 'luasnip' },
+    },
+    mapping = {
+        ['<CR>']      = cmp.mapping.confirm({ select = false }),
+        ['<C-Space>'] = cmp.mapping.complete(),
+        ['<C-j>']     = cmp_action.luasnip_jump_forward(),
+        ['<C-k>']     = cmp_action.luasnip_jump_backward(),
+        ['<C-b>']     = cmp.mapping.scroll_docs(-4),
+        ['<C-f>']     = cmp.mapping.scroll_docs(4),
+    }
+})
+
 local lsp_config = require('lspconfig')
 
-lsp_config.denols.setup {
-  on_attach = on_attach,
-  root_dir = lsp_config.util.root_pattern("deno.json", "deno.jsonc"),
-}
+lsp_config.denols.setup({
+    root_dir = lsp_config.util.root_pattern("deno.json", "deno.jsonc"),
+})
 
-lsp_config.tsserver.setup {
-  on_attach = on_attach,
-  root_dir = lsp_config.util.root_pattern("package.json"),
-  single_file_support = false
-}
+lsp_config.tsserver.setup({
+    root_dir = lsp_config.util.root_pattern("package.json"),
+    single_file_support = false
+})
