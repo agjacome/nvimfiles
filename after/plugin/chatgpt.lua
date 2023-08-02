d12a69c93740617bef16c4465bfb6aea32d91dac
Alberto G. Jácome <contact@agjacome.dev>
2023-08-02 21:08:19 +0200

    Prevent ChatGPT auto-load with a new custom enable command

--- after/plugin/chatgpt.lua
+++ after/plugin/chatgpt.lua
@@ -1,9 +1,11 @@
-local status_ok, chatgpt = pcall(require, 'chatgpt')
+vim.api.nvim_create_user_command('ChatGPTEnable', function()
+    local status_ok, chatgpt = pcall(require, 'chatgpt')
 
-if not status_ok then
-    return
-end
+    if not status_ok then
+        return
+    end
 
-chatgpt.setup({
-    api_key_cmd = "pass keys/api/openai"
-})
+    chatgpt.setup({
+        api_key_cmd = "pass keys/api/openai"
+    })
+end, { })
