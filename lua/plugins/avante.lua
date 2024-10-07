ef0dcc02c2b17e9ae53f11699f8c92cc36c08b74
Alberto G. Jácome <contact@agjacome.dev>
2024-10-07 20:10:39 +0200

    Setup Avante

---
+++ lua/plugins/avante.lua
@@ -0,0 +1,27 @@
+return {
+    "yetone/avante.nvim",
+    event = "VeryLazy",
+    lazy = false,
+    build = "make",
+    dependencies = {
+        "nvim-treesitter/nvim-treesitter",
+        "stevearc/dressing.nvim",
+        "nvim-lua/plenary.nvim",
+        "muniftanjim/nui.nvim",
+        "zbirenbaum/copilot.lua",
+    },
+    opts = {
+        provider = "claude",
+        auto_suggestions_provider = "copilot",
+        behavior = {
+            auto_suggestions = false,
+            auto_set_highlight_group = false,
+            auto_set_keymaps = true,
+            auto_apply_diff_after_generation = false,
+            support_paste_from_clipboard = false,
+        },
+        hints = {
+            enabled = false
+        }
+    }
+}
