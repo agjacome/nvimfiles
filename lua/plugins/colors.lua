050c05f853e6aff05db867386a554d966101c329
Alberto G. Jácome <alberto.jacome@frontify.com>
2024-10-11 11:29:23 +0200

    Merge lualine config to colors file

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -1,22 +1,35 @@
 return {
-    "mcchrish/zenbones.nvim",
-    dependencies = {
-        "rktjmp/lush.nvim",
+    {
+        "mcchrish/zenbones.nvim",
+        dependencies = {
+            "rktjmp/lush.nvim",
+        },
+        lazy = false,
+        config = function()
+            vim.g.zenbones = {
+                lightness = 'dim',
+                italic_comments = false,
+            }
+
+            vim.o.background = 'dark'
+            vim.cmd.colorscheme('zenbones')
+
+            local toggle_background = function()
+                vim.o.background = vim.o.background == 'dark' and 'light' or 'dark'
+            end
+
+            vim.keymap.set('n', '<f12>', toggle_background, { remap = false, desc = 'Toggle background' })
+        end,
     },
-    lazy = false,
-    config = function()
-        vim.g.zenbones = {
-          lightness = 'dim',
-          italic_comments = false,
+    {
+        "nvim-lualine/lualine.nvim",
+        lazy = false,
+        opts = {
+            options = {
+                component_separators = "|",
+                icons_enabled = false,
+                theme = "zenbones",
+            }
         }
-
-        vim.o.background = 'dark'
-        vim.cmd.colorscheme('zenbones')
-
-        local toggle_background = function()
-            vim.o.background = vim.o.background == 'dark' and 'light' or 'dark'
-        end
-
-        vim.keymap.set('n', '<f12>', toggle_background, { remap = false, desc = 'Toggle background' })
-    end,
+    }
 }
