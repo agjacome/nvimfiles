0db6ff67a78ee89853eb926e2fe2fe5160b95404
Alberto G. Jácome <contact@agjacome.dev>
2024-11-01 22:17:19 +0100

    Add keybinds for searching text under cursor with Telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -63,10 +63,13 @@ return {
         end
 
 
-        vim.keymap.set("n", "<C-p>",     builtin.git_files,  opts("Fuzzy-find git files (Telescope)"))
-        vim.keymap.set("n", "<leader>p", find_files,         opts("Fuzzy-find workspace files (Telescope)"))
-        vim.keymap.set("n", "<leader>s", builtin.live_grep,  opts("Fuzzy-find by content (Telescope)"))
-        vim.keymap.set("n", "<leader>l", builtin.treesitter, opts("Fuzzy-find by syntax (Telescope)"))
+        vim.keymap.set("n", "<C-p>",     builtin.git_files,   opts("Fuzzy-find git files (Telescope)"))
+        vim.keymap.set("n", "<leader>p", find_files,          opts("Fuzzy-find workspace files (Telescope)"))
+        vim.keymap.set("n", "<leader>s", builtin.live_grep,   opts("Fuzzy-find by content (Telescope)"))
+        vim.keymap.set("n", "<leader>S", builtin.grep_string, opts("Fuzzy-find text under cursor (Telescope)"))
+        vim.keymap.set("n", "<leader>l", builtin.treesitter,  opts("Fuzzy-find by syntax (Telescope)"))
+
+        vim.keymap.set("v", "<leader>s", "\"zy<cmd>exec 'Telescope grep_string default_text=' . escape(@z, ' ')<cr>", opts("Fuzzy-find selected text (Telescope)"))
 
         vim.keymap.set("n", "<leader>bb", builtin.buffers,                    opts("Fuzzy-find buffers (Telescope)"))
         vim.keymap.set("n", "<leader>bs", builtin.current_buffer_fuzzy_find,  opts("Fuzzy-find current buffer (Telescope)"))
