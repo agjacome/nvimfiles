96891a9ab3d337c0d0903860b8091b81d44851e1
Alberto G. Jácome <contact@agjacome.dev>
2024-12-29 18:52:25 +0100

    Cleanup keymaps

--- lua/agjacome/init.lua
+++ lua/agjacome/init.lua
@@ -1,3 +1,7 @@
+Opts = function(description)
+    return { desc = description, remap = false, silent = true }
+end
+
 require('agjacome.options')
 require('agjacome.commands')
 require('agjacome.keymaps')
--- lua/agjacome/keymaps.lua
+++ lua/agjacome/keymaps.lua
@@ -1,63 +1,45 @@
-local opts = function(desc)
-    return { desc = desc, remap = false, silent = true }
-end
+-- leaders
+vim.g.mapleader      = " "
+vim.g.maplocalleader = " "
 
-vim.g.mapleader      = ' '
-vim.g.maplocalleader = ' '
-
--- disable stuff
-vim.keymap.set('n', 'q:',      '<nop>', opts('Disabled'))
-vim.keymap.set('n', 'q/',      '<nop>', opts('Disabled'))
-vim.keymap.set('n', 'q?',      '<nop>', opts('Disabled'))
-vim.keymap.set('n', 'Q',       '<nop>', opts('Disabled'))
-vim.keymap.set('n', '<left>',  '<nop>', opts('Disabled'))
-vim.keymap.set('n', '<right>', '<nop>', opts('Disabled'))
-vim.keymap.set('n', '<up>',    '<nop>', opts('Disabled'))
-vim.keymap.set('n', '<down>',  '<nop>', opts('Disabled'))
+-- disabled
+vim.keymap.set("n", "q:",      "<nop>", Opts("Disabled"))
+vim.keymap.set("n", "q/",      "<nop>", Opts("Disabled"))
+vim.keymap.set("n", "q?",      "<nop>", Opts("Disabled"))
+vim.keymap.set("n", "Q",       "<nop>", Opts("Disabled"))
+vim.keymap.set("n", "<left>",  "<nop>", Opts("Disabled"))
+vim.keymap.set("n", "<right>", "<nop>", Opts("Disabled"))
+vim.keymap.set("n", "<up>",    "<nop>", Opts("Disabled"))
+vim.keymap.set("n", "<down>",  "<nop>", Opts("Disabled"))
 
 -- navigation
-vim.keymap.set('n', '<c-h>',   '<c-w>h',         opts('Move to Left window'))
-vim.keymap.set('n', '<c-j>',   '<c-w>j',         opts('Move to Down window'))
-vim.keymap.set('n', '<c-k>',   '<c-w>k',         opts('Move to Up window'))
-vim.keymap.set('n', '<c-l>',   '<c-w>l',         opts('Move to Right window'))
-vim.keymap.set('n', '<tab>',   ':bnext<cr>',     opts('Move to Next buffer'))
-vim.keymap.set('n', '<s-tab>', ':bprevious<cr>', opts('Move to Previous buffer'))
-
--- quickfix navigation
-vim.api.nvim_create_autocmd('BufReadPost', {
-    group   = vim.api.nvim_create_augroup('quickfix', {}),
-    pattern = 'quickfix',
-    command = 'nnoremap <buffer> <cr> <cr>'
-})
-
--- toggles
-local toggle_hlsearch = function()
-    vim.o.hlsearch = not vim.o.hlsearch
-end
-
-local toggle_relnum = function()
-    vim.o.relativenumber = not vim.o.relativenumber
-end
-
-vim.keymap.set('n', '<cr>', toggle_hlsearch, opts('Toggle search Highlight'))
-vim.keymap.set('n', '<f6>', toggle_relnum,   opts('Toggle Relative line number'))
-
--- expansions
-vim.keymap.set('c', '%%',        '<c-r>=expand("%:p:h") . "/"<cr>',   opts('Expand to current directory'))
-vim.keymap.set('n', '<leader>D', '"=strftime("%Y-%m-%d %H:%M")<cr>p', opts('Insert current DateTime'))
+vim.keymap.set("n", "<tab>",   ":bnext<cr>",     Opts("Next buffer"))
+vim.keymap.set("n", "<s-tab>", ":bprevious<cr>", Opts("Previous buffer"))
 
 -- diagnostics
-vim.keymap.set('n', 'gl', vim.diagnostic.open_float, opts('Show line diagnostics'))
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev,  opts('Go to previous diagnostic'))
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next,  opts('Go to next diagnostic'))
+vim.keymap.set("n", "gl", vim.diagnostic.open_float, Opts("Show line diagnostics"))
+vim.keymap.set("n", "[d", vim.diagnostic.goto_prev,  Opts("Go to previous diagnostic"))
+vim.keymap.set("n", "]d", vim.diagnostic.goto_next,  Opts("Go to next diagnostic"))
 
--- tmux stuff
+-- quickfix
+vim.api.nvim_create_autocmd("BufReadPost", {
+    group   = vim.api.nvim_create_augroup("quickfix", {}),
+    pattern = "quickfix",
+    command = "nnoremap <buffer> <cr> <cr>"
+})
+
+-- custom
+vim.keymap.set("n", "<leader>X", "<cmd>source %<cr>", Opts("Source current file"))
+vim.keymap.set("v", "<leader>X", ":lua<cr>", Opts("Source current selection"))
+vim.keymap.set("n", "<leader>D", '"=strftime("%Y-%m-%d %H:%M")<cr>p', Opts("Insert current datetime"))
+vim.keymap.set("n", "<leader><cr>", function() vim.o.hlsearch = not vim.o.hlsearch end, Opts("Toggle search highlight"))
+
 local tmux_session = function()
     if vim.env.TMUX then
-        vim.fn.system('tmux split-window -v -l 20 "tmux-session"')
+        vim.fn.system("tmux split-window -v -l 20 tmux-session")
     else
-        print('Not in a tmux session')
+        print("Not in a tmux session")
     end
 end
 
-vim.keymap.set('n', '<c-t>', tmux_session, opts('Open tmux session'))
+vim.keymap.set("n", "<c-t>", tmux_session, Opts("Tmux session"))
