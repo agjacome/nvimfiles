3cfc9a833847886df91a95566aa8bcf9d1783f16
Alberto G. Jácome <alberto.jacome@frontify.com>
2025-07-22 22:32:11 +0200

    Remove easy align and gitsigns plugins

--- lua/plugins/easyalign.lua
+++
@@ -1,7 +0,0 @@
-return {
-    "junegunn/vim-easy-align",
-    event = "VeryLazy",
-    config = function()
-        vim.keymap.set("v", "<cr>",  ":LiveEasyAlign<cr>", Opts("EasyAlign - Start"))
-    end
-}
