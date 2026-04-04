d18653e1600590ea8e7f9f2eeeda2e9ed6b1cc44
Alberto G. Jácome <contact@agjacome.dev>
2026-04-04 19:17:15 +0200

    refactor(telescope): switch to default theme with cursor for ui-select

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -11,20 +11,14 @@ return {
         local telescope = require("telescope")
         local actions = require("telescope.actions")
 
-        telescope.load_extension("fzf")
-        telescope.load_extension("ui-select")
-
         telescope.setup({
-            defaults = require('telescope.themes').get_ivy({
+            defaults = {
                 path_display = { "shorten" },
                 layout_config = {
-                    height = 40,
+                    width = 0.9,
+                    height = 0.8,
                 },
-                borderchars = {
-                    prompt = { "─", "│", " ", "│", "┌", "┐", "│", "│" },
-                    results = { "─", "│", "─", "│", "├", "┤", "┘", "└" },
-                    preview = { "─", "│", "─", "│", "┌", "┐", "┘", "└" },
-                },
+                borderchars = { "─", "│", "─", "│", "┌", "┐", "┘", "└" },
                 vimgrep_arguments = {
                     "rg",
                     "--color=never",
@@ -43,13 +37,27 @@ return {
                         ["<C-r>"] = actions.cycle_previewers_prev,
                     },
                 },
-            }),
+            },
             pickers = {
                 colorscheme = {
                     enable_preview = true
                 }
+            },
+            extensions = {
+                ["ui-select"] = {
+                    require('telescope.themes').get_cursor({
+                        borderchars = {
+                            prompt = { "─", "│", " ", "│", "┌", "┐", "│", "│" },
+                            results = { "─", "│", "─", "│", "├", "┤", "┘", "└" },
+                            preview = { "─", "│", "─", "│", "┌", "┐", "┘", "└" },
+                        },
+                    }),
+                },
             }
         })
+
+        telescope.load_extension("fzf")
+        telescope.load_extension("ui-select")
 
         local builtin = require("telescope.builtin")
 
@@ -64,7 +72,10 @@ return {
         vim.keymap.set("n", "<leader>p", find_files,          Opts("Telescope - Workspace files"))
         vim.keymap.set("n", "<leader>s", builtin.live_grep,   Opts("Telescope - Content"))
         vim.keymap.set("n", "<leader>S", builtin.grep_string, Opts("Telescope - Word under cursor"))
-        vim.keymap.set("v", "<leader>s", "\"zy<cmd>exec 'Telescope live_grep default_text=' . escape(@z, ' ')<cr>", Opts("Telescope - Selected text"))
+        vim.keymap.set("v", "<leader>s", function()
+            local text = table.concat(vim.fn.getregion(vim.fn.getpos("v"), vim.fn.getpos(".")), "\n")
+            builtin.live_grep({ default_text = text })
+        end, Opts("Telescope - Selected text"))
 
         vim.keymap.set("n", "<leader>bb", builtin.buffers, Opts("Telescope - Buffers"))
 
